75162b3ee9c0ee2a5870d9f4f24d87852b07c69d
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-09-11 15:14:50 +0300

    add

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <Provider store={store}>
-        <BrowserRouter  basename="/goit-lms-career-homework">
+        <BrowserRouter >
           <App />
         </BrowserRouter >
         </Provider>
