e33aaba33eafb4a6b4920ed3112de2bc02f33c7d
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-09-13 12:48:08 +0300

    add  form style

--- src/components/pages/Catalog/Catalog.jsx
+++ src/components/pages/Catalog/Catalog.jsx
@@ -7,27 +7,19 @@ import { fetchAllCards, fetchCards } from 'redux/cards/operetions';
 import { Loader } from 'components/elements/Loader/Loader';
 import { DetailedCard } from './DetailedCard/DetailedCard';
 import { Modal } from 'components/elements/Modal/Modal';
+import { Filter } from 'components/elements/Filter/Filter';
 
 export const Catalog = () => {
   const dispatch = useDispatch() 
-
   const [showModal, setShowModal] = useState(false);
   const toggleModal = () => setShowModal(prevShowModal => !prevShowModal);
-  let [loading, setLoading] = useState(false);
-
-  const [selectedCarBrand, setSelectedCarBrand] = useState('');
-  const [selectedCarPrice, setSelectedCarPrice] = useState('');
-  // eslint-disable-next-line
-  const [priceSelected, setPriceSelected] = useState(''); 
-  const [mileageFrom, setMileageFrom] = useState('');
-  const [mileageTo, setMileageTo] = useState('');
+  const [loading, setLoading] = useState(false);
   const [cardToOpen, setCardToOpen] = useState([]);
   const [isButtonVisible, setButtonVisibility] = useState('Catalog__btn--more');
   const [page, setPage] = useState(1);
-  // let cardList = [];
+ 
   
-  
-// GET CARS
+// GET CARDS LIST
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
@@ -43,7 +35,6 @@ const cardList = useSelector(getCards);
 const numberOfCards = useSelector(cardsInList);
 
 
-
   // Modal for DetailedCard
   const openModal = (card) => {
     setCardToOpen(card);
@@ -56,9 +47,8 @@ const numberOfCards = useSelector(cardsInList);
 
   // Get NEXT PAGE
 const getnextPage = (e) => {
-  const pages = numberOfCards / 8;
-  
   e.preventDefault();
+  const pages = numberOfCards / 8;
   setPage(page + 1);
   dispatch(fetchCards(page+1));
   if ((page + 1) >= pages ) {
@@ -66,113 +56,26 @@ const getnextPage = (e) => {
   } else {
     setButtonVisibility('Catalog__btn--more');
   }
-
-
 }
 
 
   // INPUT List of car brands
   const carBrands = [...new Set(cardList.map(car => car.make))];
-
-  const handleInputChange = (event) => {
-    setSelectedCarBrand(event.target.value);
-  };
-
-
  // INPUT List of car price
  const carPrice = [...new Set(cardList.map(car => car.rentalPrice.replace(/\$/g, '')))];
  
-  const handleInputChangePrice = (event) => {
-    setPriceSelected(event.target.value);
-    setSelectedCarPrice(`To ${event.target.value} $`);
-  };
 
 
-
-  const handleMileageFromChange = (event) => {
-    setMileageFrom(event.target.value);
-  };
-
-  const handleMileageToChange = (event) => {
-    setMileageTo(event.target.value);
-  };
-
-  const setFavorite = (e) => {
-    
-  }
+  const setFavorite = (e) => {}
 
   return (
     <div className='Catalog__container'>
-      {/* Header */}
-      <div className='Catalog__header'>
-            {/* Car Brands  */}
-            <div className='Catalog__input'>
-            <label htmlFor="carBrandInput">Car Brand</label>
-            <input
-              type="text"
-              id="carBrandInput"
-              list="carBrands"
-              value={selectedCarBrand}
-              placeholder='Enter the text'
-              onChange={handleInputChange}
-            />
-            <datalist id="carBrands">
-            {carBrands.map((brand, index) => (
-              <option key={index} value={brand} />
-            ))}
-          </datalist>
-          </div>
-
-
-          {/* Price  */}
-          <div className='Catalog__input'>
-            <label htmlFor="carPriceInput">Price/ 1 hour</label>
-            <input
-              type="text"
-              id="carPriceInput"
-              list="carPrice"
-              value={selectedCarPrice}
-              placeholder='To $'
-              onChange={handleInputChangePrice}
-            />
-            <datalist id="carPrice">
-            {carPrice.map((price, index) => (
-              <option key={index} value={price} />
-            ))}
-          </datalist>
-          </div>
-
-          {/* Сar mileage */}
-          <div  className='Catalog__input'>
-            <label htmlFor="mileageFromInput">Сar mileage / km</label>
-            <div>
-            <input
-              type="number"
-              id="mileageFromInput"
-              value={mileageFrom}
-              onChange={handleMileageFromChange}
-              className='Catalog__input-mileage-left'
-              placeholder='From'
-            />
-
-            <input
-              type="number"
-              id="mileageFromInput"
-              value={mileageTo}
-              onChange={handleMileageToChange}
-              className='Catalog__input-mileage-right'
-              placeholder='To'
-            />
-            </div>
-          </div>
-
-          <button type='submite' className='Catalog__btn'>
-          <span className="btn-text-one">Search</span>
-           <span className="btn-text-two">Go!</span>         
-            </button>
-
-
-      </div>   
+      {/* Header Filter */}
+      <Filter 
+        carBrands={carBrands}
+        carPrice ={carPrice}
+        />
+        
       {/* Catalog */}
         {loading ? 
           <Loader className='Catalog__loader'/>
